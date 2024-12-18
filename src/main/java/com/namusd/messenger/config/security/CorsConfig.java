745e9ee1fb5fac429d480aa4a53e3941b0dd69ba
thekim123 <thekim12@nate.com>
2024-12-18 19:31:33 +0900

    feat: cors 설정 변경

--- src/main/java/com/namusd/messenger/config/security/CorsConfig.java
+++ src/main/java/com/namusd/messenger/config/security/CorsConfig.java
@@ -2,6 +2,7 @@ package com.namusd.messenger.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -15,12 +16,27 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        config.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
-                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedMethod("GET, POST, DELETE, PUT");
+//        config.setAllowedHeaders(Arrays.asList("*"));
+
+        config.setAllowedHeaders(
+                Arrays.asList(
+                        "X-Requested-With",
+                        "Origin",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                        "strict-origin-when-cross-origin"
+                ));
+        config.setExposedHeaders(
+                Arrays.asList(
+                        "Access-Control-Allow-Headers",
+                        "Authorization, x-xsrf-token",
+                        "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With",
+                        "Content-Type", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers"
+                ));
+        config.addAllowedOrigin("http://localhost:5173");
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
